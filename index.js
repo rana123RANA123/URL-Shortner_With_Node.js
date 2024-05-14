7d66fef3ea40003da839bda0d8d2f638732aa088
Rana Asad Ali <ranaasadaly@gmail.com>
2024-05-14 17:57:49 +0500

    First Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const { connectToMongoDB } = require("./connect");
+const urlRoute = require("./routes/url");
+const URL = require("./models/url");
+
+const app = express();
+const PORT = 8001;
+
+connectToMongoDB("mongodb+srv://nodejspractice:nodejspractice@cluster0.ogrx2lt.mongodb.net/url?retryWrites=true&w=majority&appName=Cluster0").then(() =>
+  console.log("Mongodb connected")
+);
+
+app.use(express.json());
+
+app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+  res.redirect(entry.redirectURL);
+});
+
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
